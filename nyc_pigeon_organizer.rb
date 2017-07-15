d62a5c25b8c2b54110b71aab689b65c0a8dd7589
mthorry <mthorry@gmail.com>
2017-07-15 15:24:02 -0400

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_data = {}
+
+  data.each do |keys, values|
+    values.each do |traits, names|
+      names.each do |name|
+      # binding.pry
+        pigeon_data[name] ||= {} #creates a new KEY for the pigeon_data hash for each pigeon name
+        pigeon_data[name][keys] ||= [] #creates a new VALUES ARRAY using the KEYS from original data WITHIN the new NAME KEY
+        pigeon_data[name][keys] << traits.to_s #addes each KEY to the new VALUES ARRAY as a string
+      end
+    end
+  end
+  pigeon_data
 end
